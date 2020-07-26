1ea0993d12a7251025b580832a97faf0215f79f0
Jenny Dinh <jennythi.d@gmail.com>
2020-07-26 16:32:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+let date = new Date();
+    console.log(date);
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let second = date.getSeconds();
+
+    let hrPosition = (hr*360/12)+(min*(360/60)/12);
+    let minPosition = (min*360/60)+(second*(360/60)/60);
+    let secPosition = second*360/60;
+
+function runClock() {
+
+    hrPosition = hrPosition+(3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+6;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+let interval = setInterval(runClock, 1000);
